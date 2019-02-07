5e254226f1df363209cae88bc8496451d859fcf5
Alla <allamets1607@gmail.com>
2019-02-07 17:02:27 +0200

    Ex.2 Points

--- sandbox/src/main/java/ru/sqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/sqa/pft/sandbox/Point.java
@@ -1,4 +1,11 @@
 package ru.sqa.pft.sandbox;
 
 public class Point {
+
+    double x;
+    double y;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 }
--- sandbox/src/main/java/ru/sqa/pft/sandbox/test.java
+++ sandbox/src/main/java/ru/sqa/pft/sandbox/test.java
@@ -4,15 +4,29 @@ public class test {
 
     public static void main(String[] args) {
 
-        NewSquare s = new NewSquare(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area(s));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
         Rectangle r = new Rectangle(4,5);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area(r));
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+
+        Point obj1 = new Point(5,4);
+        Point obj2 = new Point(6,10);
+        System.out.println("Distance: " + "A(" + obj1.x + ":" + obj1.y + ") B(" + obj2.x + ":" + obj2.y + ")" + "=" + point(obj1, obj2));
     }
 
-    public static  double area(NewSquare s){
+    public static double point(Point obj1, Point obj2){
+        //Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        double distance = Math.sqrt((obj2.x-obj1.x)*(obj2.x-obj1.x)+(obj2.y-obj1.y)*(obj2.y-obj1.y));
+        return distance;
+    }
+
+    public static  double area(Square s){
         return s.l * s.l;
+    }
+
+    public static  double area(Rectangle r){
+        return r.a * r.b;
     }
 
 }
