bf027fa9b9b4e91522badb77a87f0a3d79513299
Alla <42247433+allamets95@users.noreply.github.com>
2019-02-22 18:08:11 +0200

    доработка 4 задания

--- addressbook-web-tests/src/test/java/lessons/tests/addressbook/ContactAddTests.java
+++ addressbook-web-tests/src/test/java/lessons/tests/addressbook/ContactAddTests.java
@@ -2,6 +2,7 @@ package lessons.tests.addressbook;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,7 +12,7 @@ public class ContactAddTests {
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
-        wd = new FirefoxDriver();
+        wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         login("admin", "secret");
@@ -21,7 +22,7 @@ public class ContactAddTests {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='User:'])[1]/following::label[1]")).click();
+        //wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='User:'])[1]/following::label[1]")).click();
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys(password);
@@ -48,14 +49,14 @@ public class ContactAddTests {
     }
 
     private void saveNewContact() {
-        wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
+        wd.findElement(By.xpath("//*[@id=\"content\"]/form/input[1]")).click();
     }
 
     private void fillNewContact(ContactData contactData) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
         wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-        wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='First name:'])[1]/following::label[1]")).click();
+        //wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='First name:'])[1]/following::label[1]")).click();
         wd.findElement(By.name("middlename")).click();
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
@@ -66,7 +67,7 @@ public class ContactAddTests {
         wd.findElement(By.name("home")).click();
         wd.findElement(By.name("home")).clear();
         wd.findElement(By.name("home")).sendKeys(contactData.getHome());
-        wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Mobile:'])[1]/following::label[1]")).click();
+        //wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Mobile:'])[1]/following::label[1]")).click();
         wd.findElement(By.name("mobile")).click();
         wd.findElement(By.name("mobile")).clear();
         wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
